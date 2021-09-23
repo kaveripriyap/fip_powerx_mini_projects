d4fcb979b989a00ef6002fd467215e20a6e87b6d
Kaveri Priya Putti <kaveripriyap2312@gmail.com>
2021-09-24 01:28:04 +0800

    Update library.py

--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -4,7 +4,11 @@ import random
 array = []
 
 def gen_random_int(number, seed):
-	pass
+    my_list = [i for i in range(number)]
+    random.seed(seed)
+    random.shuffle(my_list)
+    result = my_list
+    return result
 
 def generate():
 	global array
@@ -12,22 +16,25 @@ def generate():
 	number = 10
 	seed = 200
 
+	gen_random_int(number, seed)
+
 	# call gen_random_int() with the given number and seed
 	# store it to the global variable array
-	pass
 
-	array = None
+	array = gen_random_int(number, seed)
+
 	# convert the items into one single string 
 	# the number should be separated by a comma
 	# and a full stop should end the string.
-	pass
+	
+	array_str = ','.join([str(i) for i in array]) + '.'
 
-	array_str = None
+	console.log(array, "\n", array_str)
 
 	# This line is to placed the string into the HTML
 	# under div section with the id called "generate"	
 	document.getElementById("generate").innerHTML = array_str
-
+	
 
 def sortnumber1():
 	'''	This function is used in Exercise 1.
@@ -39,11 +46,18 @@ def sortnumber1():
 		- call your sort function, either bubble sort or insertion sort
 		- create a string of the sorted numbers and store it in array_str
 	'''
-	pass
+	n = len(array)
+	for i in range(1, n):
+		for j in range(1, n):
+			first_num_index = j - 1
+			second_num_index = j
+			if array[first_num_index] > array[second_num_index]:
+				array[first_num_index], array[second_num_index] = array[second_num_index], array[first_num_index]
 
-	array_str = None
-	
+	array_str = ','.join([str(i) for i in array]) + '.'
+		
 	document.getElementById("sorted").innerHTML = array_str
+
 
 def sortnumber2():
 	'''	This function is used in Exercise 2.
@@ -63,13 +77,24 @@ def sortnumber2():
 	if value == "":
 		window.alert("Your textbox is empty")
 		return
+	else:
+		value = value.split(",")
 
 	# Your code should start from here
 	# store the final string to the variable array_str
-	pass
+	
+	n = len(array)
+	for i in range(1, n):
+		for j in range(1, n):
+			first_num_index = j - 1
+			second_num_index = j
+			if array[first_num_index] > array[second_num_index]:
+				array[first_num_index], array[second_num_index] = array[second_num_index], array[first_num_index]
 
-	array_str = None
+	if array == "":
+		window.alert("Your textbox is empty")
+		return
+
+	array_str = ','.join([str(i) for i in array]) + '.'
 
 	document.getElementById("sorted").innerHTML = array_str
-
-
